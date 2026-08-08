17eadbf5cd6c654b6d78dabaca6cda5dfd5f8b70
Samuel Hyman <Samuel.L.Hyman@gmail.com>
2026-08-08 10:29:47 -0500

    refactor: clean up dead RoutingContext class and staging changes

--- common/network/src/main/java/catalyst/common/network/GatewayFrameEncoder.java
+++ common/network/src/main/java/catalyst/common/network/GatewayFrameEncoder.java
@@ -8,7 +8,7 @@ public final class GatewayFrameEncoder extends MessageToByteEncoder<GatewayFrame
 
     @Override
     protected void encode(ChannelHandlerContext ctx, GatewayFrame msg, ByteBuf out) throws Exception {
-        // 1. Write message flag
+        // 1. Write the message flag
         out.writeByte(msg.flag());
 
         // 2. Write sessionId length & bytes
